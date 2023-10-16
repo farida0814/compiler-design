e27a9bb0636406e3b94f97590bf9e861572501ec
farida0814 <122246329+farida0814@users.noreply.github.com>
2023-10-16 10:35:31 +0530

    Add files via upload

---
+++ prog 3 ignore the redudant.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+ 
+int isKeyword(char buffer[]){
+char keywords[32][10] = {"main","auto","break","case","char","const","continue","default",
+"do","double","else","enum","extern","float","for","goto",
+"if","int","long","register","return","short","signed",
+"sizeof","static","struct","switch","typedef",
+"unsigned","void","printf","while"};
+int i, flag = 0;
+}
+fclose(fp);
+return 0;
+}
+
+Input: 3lex_input.txt
+main ( )
+{
+  int a, b, c ;
+  c = b + c;
+  printf ( "%d" ,c ) ;
+  }
